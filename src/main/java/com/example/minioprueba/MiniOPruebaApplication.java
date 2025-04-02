ed388831c0e9c13e6df073a0f8140766c03c839e
Garbi-Collector <uwurulos@gmail.com>
2025-04-02 12:31:29 -0300

    commit inicial

---
+++ src/main/java/com/example/minioprueba/MiniOPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.minioprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniOPruebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniOPruebaApplication.class, args);
+    }
+
+}
